75484a00cc9bb7300e678dae5e124816983796bf
mufus <magnus@fanglobe.se>
2016-11-29 22:50:16 +0100

    Update after-pdns-sync.sh

--- hub/after-pdns-sync.sh
+++ hub/after-pdns-sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sleep 1
-cp -f /home/hubsync/powerdns/pdns.sqlite3 /var/lib/powerdns/pdns.sqlite3 && service pdns reload
+cp -f /home/hubsync/powerdns/pdns.sqlite3 /var/lib/powerdns/pdns.sqlite3 && service pdns restart
