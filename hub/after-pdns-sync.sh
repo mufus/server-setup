77aef64eb195102e6bd6bc788ec27103a81cc616
mufus <magnus@fanglobe.se>
2022-07-04 19:13:29 +0200

    Fix for debian buster

--- hub/after-pdns-sync.sh
+++ hub/after-pdns-sync.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 sleep 1
-cp -f /home/hubsync/powerdns/pdns.sqlite3 /var/lib/powerdns/pdns.sqlite3 && service pdns restart
+cp -f /home/hubsync/powerdns/pdns.sqlite3 /var/lib/powerdns/pdns.sqlite3 && chown pdns: /var/lib/powerdns/pdns.sqlite3 && service pdns restart
